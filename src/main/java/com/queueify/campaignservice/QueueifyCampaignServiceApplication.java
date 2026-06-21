d7e4044d51e8ed028b8d69c1dceeb42235407a7e
Utkarsh Rajput <utkarshrajput@Utkarshs-MacBook-Air.local>
2026-06-21 19:53:42 +0530

    Project setup

---
+++ src/main/java/com/queueify/campaignservice/QueueifyCampaignServiceApplication.java
@@ -0,0 +1,13 @@
+package com.queueify.campaignservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueifyCampaignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueifyCampaignServiceApplication.class, args);
+	}
+
+}
